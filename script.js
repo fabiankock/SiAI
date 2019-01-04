c927741603504f4564757abe57bcf86d4231e0b6
Fabian Kock <f.kock@web.de>
2019-01-04 11:50:59 +0100

    Added Button Interaction

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ $('.firmenlogo').bind('mouseover', function(event) {
   $('.review_text').hide();
   $('.'+event.currentTarget.id).show();
 })
+
+$(document).ready(function() {
+  $(".test_btn").click(function(){
+      alert("Test Button");
+  });
+  
+  $('.contact_btn').click(function(){
+      alert("Contact Button");
+  });
+});
